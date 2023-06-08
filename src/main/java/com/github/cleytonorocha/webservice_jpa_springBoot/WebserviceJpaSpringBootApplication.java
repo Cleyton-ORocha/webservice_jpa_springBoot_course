3d6b91071cd808361e19763f58011a8a58941207
Cleyton <cleytoodeoliveirarocha@gmail.com>
2023-06-08 08:45:56 -0300

    initial commit

---
+++ src/main/java/com/github/cleytonorocha/webservice_jpa_springBoot/WebserviceJpaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.github.cleytonorocha.webservice_jpa_springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceJpaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceJpaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/cleytonorocha/webservice_jpa_springBoot/WebserviceJpaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cleytonorocha.webservice_jpa_springBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceJpaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
